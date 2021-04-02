be4593ce01ce1c403f74ee4080753a01fb3142a8
Maximilien <59966496+Max-RDS@users.noreply.github.com>
2021-04-02 11:23:31 +0200

    Add files via upload

---
+++ PPE2_Appli_Frais/controleurs/c_ajoutFrais.php
@@ -0,0 +1,4 @@
+<?php
+include("vues/v_sommaire.php");
+require_once("include/config.php");
+require_once("include/class.pdogsb.inc.php");
---
+++ PPE2_Appli_Frais/index.php
@@ -0,0 +1,41 @@
+<?php
+require_once("include/fct.inc.php");
+require_once("include/class.pdogsb.inc.php");
+include("vues/v_entete.php") ;
+session_start();
+
+?>
+	<link rel="StyleSheet" href="styles/bootstrap.css" />
+
+	<div id="rectangle_1"  ><?php
+  $pdo = PdoGsb::getPdoGsb();
+  $estConnecte = estConnecte();
+
+  if(!isset($_REQUEST['uc']) || !$estConnecte){
+       $_REQUEST['uc'] = 'connexion';
+  }
+  $uc = $_REQUEST['uc'];
+  switch($uc){
+    case 'connexion':{
+      include("controleurs/c_connexion.php");break;
+    }
+    case 'gererFrais' :{
+      include("controleurs/c_gererFrais.php");break;
+    }
+    case 'etatFrais' :{
+      include("controleurs/c_etatFrais.php");break;
+    }
+    case 'validerFrais' :{
+      include("controleurs/c_validerFrais.php");break;
+    }
+    case 'validerfichefrais' :{
+
+      include("controleurs/c_validerFrais.php");break;
+    }
+    case 'ajoutFrais' :{
+      include("controleurs/c_ajoutFrais.php");break;
+    }
+  }
+  include("vues/v_pied.php") ;
+  ?></div>
+	<div id="rectangle_2"  ></div>
